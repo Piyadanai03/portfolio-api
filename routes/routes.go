367514e8341ab5fcca3f828aff1bced05eafff4b
Piyadanai03 <piyadnai.k2546@gmail.com>
2026-04-12 08:59:57 +0700

    feat: add CORS middleware with environment variables

--- routes/routes.go
+++ routes/routes.go
@@ -1,20 +1,38 @@
 package routes
 
 import (
-	_ "github.com/Piyadanai03/portfolio-api/docs"
-	"github.com/gin-gonic/gin"
 	"github.com/Piyadanai03/portfolio-api/controllers/auth"
-	"github.com/Piyadanai03/portfolio-api/controllers/projects"
-	"github.com/Piyadanai03/portfolio-api/middleware"
-	"github.com/swaggo/files"
-	"github.com/swaggo/gin-swagger"
 	"github.com/Piyadanai03/portfolio-api/controllers/education"
 	"github.com/Piyadanai03/portfolio-api/controllers/experience"
-	
+	"github.com/Piyadanai03/portfolio-api/controllers/projects"
+	_ "github.com/Piyadanai03/portfolio-api/docs"
+	"github.com/Piyadanai03/portfolio-api/middleware"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"os"
+	"strings"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	originsEnv := os.Getenv("ALLOWED_ORIGINS")
+	allowedOrigins := []string{}
+
+	if originsEnv != "" {
+		allowedOrigins = strings.Split(originsEnv, ",")
+	} else {
+		allowedOrigins = []string{"http://localhost:5173"}
+	}
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     allowedOrigins,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	v1 := r.Group("/api/v1")
 
@@ -24,7 +42,7 @@ func SetupRouter() *gin.Engine {
 	admin := v1.Group("/admin")
 	admin.Use(middleware.AuthMiddleware())
 	{
-		
+
 	}
 
 	member := v1.Group("/member")
